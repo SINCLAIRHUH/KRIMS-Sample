b9ee1780ab5b3e77c626ad112651646641b2807d
Sinclair, Huh <40238401+SINCLAIRHUH@users.noreply.github.com>
2023-12-11 10:18:47 +0900

    2023-12-11 : 폰트 변경

--- src/Fonts.tsx
+++ src/Fonts.tsx
@@ -8,56 +8,56 @@ export default function Fonts() {
   return (
     <style
       dangerouslySetInnerHTML={{
-        __html: `
+        __html: ` 
             @font-face {
-              font-family: 'DM Sans';
-              font-style: italic;
-              font-weight: 400;
-              font-display: swap;
-              src: url('${
-                prefix + '/fonts/dm-sans/DMSans-Italic.ttf'
-              }') format('truetype');
+              font-family: 'Noto Sans KR';
+              font-style: normal;
+              font-weight: 100;
+              src: url(/fonts/NotoSansKR-Thin.woff2) format('woff2'),
+                  url(/fonts/NotoSansKR-Thin.woff) format('woff'),
+                  url(/fonts/NotoSansKR-Thin.otf) format('opentype');
             }
-            
             @font-face {
-              font-family: 'DM Sans';
-              font-style: italic;
-              font-weight: 700;
-              font-display: swap;
-              src: url('${
-                prefix + '/fonts/dm-sans/DMSans-BoldItalic.ttf'
-              }') format('truetype');
+              font-family: 'Noto Sans KR';
+              font-style: normal;
+              font-weight: 300;
+              src: url(/fonts/NotoSansKR-Light.woff2) format('woff2'),
+                  url(/fonts/NotoSansKR-Light.woff) format('woff'),
+                  url(/fonts/NotoSansKR-Light.otf) format('opentype');
             }
-            
             @font-face {
-              font-family: 'DM Sans';
+              font-family: 'Noto Sans KR';
               font-style: normal;
               font-weight: 400;
-              font-display: swap;
-              src: url('${
-                prefix + '/fonts/dm-sans/DMSans-Regular.ttf'
-              }) format('truetype');
+              src: url(/fonts/NotoSansKR-Regular.woff2) format('woff2'),
+                    url(/fonts/NotoSansKR-Regular.woff) format('woff'),
+                    url(/fonts/NotoSansKR-Regular.otf) format('opentype');
             }
-            
             @font-face {
-              font-family: 'DM Sans';
+              font-family: 'Noto Sans KR';
               font-style: normal;
               font-weight: 500;
-              font-display: swap;
-              src: url('${
-                prefix + '/fonts/dm-sans/DMSans-Medium.ttf'
-              }) format('truetype');
+              src: url(/fonts/NotoSansKR-Medium.woff2) format('woff2'),
+                    url(/fonts/NotoSansKR-Medium.woff) format('woff'),
+                    url(/fonts/NotoSansKR-Medium.otf) format('opentype');
             }
-            
             @font-face {
-              font-family: 'DM Sans';
+              font-family: 'Noto Sans KR';
               font-style: normal;
               font-weight: 700;
-              font-display: swap;
-              src: url('${
-                prefix + '/fonts/dm-sans/DMSans-Bold.ttf'
-              }) format('truetype');
+              src: url(/fonts/NotoSansKR-Bold.woff2) format('woff2'),
+                    url(/fonts/NotoSansKR-Bold.woff) format('woff'),
+                    url(/fonts/NotoSansKR-Bold.otf) format('opentype');
             }
+            @font-face {
+              font-family: 'Noto Sans KR';
+              font-style: normal;
+              font-weight: 900;
+              src: url(/fonts/NotoSansKR-Black.woff2) format('woff2'),
+                    url(/fonts/NotoSansKR-Black.woff) format('woff'),
+                    url(/fonts/NotoSansKR-Black.otf) format('opentype');
+            } 
+        
           `,
       }}
     />
